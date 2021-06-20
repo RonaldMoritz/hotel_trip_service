75ebd6ae89d4fa14d89fa5043e8c0a26f879ee10
RonaldMoritz <ronald_moritz@yahoo.com>
2021-06-20 13:27:41 +0200

    http adresse geändert

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8083/api",
+    baseURL: "https://hoteltripservice.azurewebsites.net/api",
     headers: {
         "Content-type": "application/json"
     }
